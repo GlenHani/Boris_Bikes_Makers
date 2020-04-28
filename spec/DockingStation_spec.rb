453b75c0be6204f0d6061a972b14533c65eb44b9
GlenHani <glen.hani@beis.gov.uk>
2020-04-28 15:21:41 +0100

    Section 9 Complete

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,7 @@
 require './lib/docking_station'
 
 describe DockingStation do
-  it 'Test_1' do
+  it 'Rlease Bike' do
     docking_station = DockingStation.new
     docking_station.release_bike 
   end
